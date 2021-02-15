8d98dacdadd7b489b79d7f9ac60f499a4ca22137
Andrey <agpopov@appline.ru>
2021-02-15 11:15:27 +0300

    Задание 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,15 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
-        double a = 36.187;
-        byte b = 127;
+        int[] arr = {1, 2, 3, 4, 5};
+        int one = arr[0];
+        int i = arr.length - 1;
+        int fin = arr[i];
+        arr[0] = fin;
+        arr[i] = one;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[4]);
     }
 }
